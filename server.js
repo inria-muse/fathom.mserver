fff2cc7174b342524acfaa340716b97fa756093c
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-03-23 16:58:12 +0100

    reverse ping and traceroute

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var os = require('os');
+var exec = require('child_process').exec;
+
 var debug = require('debug')('mserver')
 var _ = require('underscore');
 var argv = require('minimist')(process.argv.slice(2));
@@ -5,10 +8,9 @@ var redis = require('redis');
 var express = require('express');
 var bodyParser = require('body-parser');
 var ipaddr = require('ipaddr.js');
-var exec = require('child_process').exec;
 var whois = require('node-whois');
-var utils = require('utils');
-var os = require('os');
+
+var utils = require('./utils');
 
 if (argv.h) {
     console.log("Usage: " + process.argv[0] + 
@@ -108,9 +110,11 @@ var handlegeo = function(req, res, ip) {
 
 	    // reverse dnsname for the IP
 	    exec('host ' + ip, function(error, stdout, stderr) {
-		if (!error && stdout) {
+		if (!error && stdout && stdout.indexOf('not found')<0) {
 		    var tokens = stdout.trim().split(' ');
-		    obj.dnsname = (tokens.lenght > 1 ? tokens[tokens.length-1] : undefined);
+		    obj.dnsname = (tokens.lenght > 1 ? 
+				   tokens[tokens.length-1] : 
+				   undefined);
 		}
 		res.status(200).send(obj);
 	    });
@@ -140,58 +144,115 @@ app.all('/geo', function(req, res){
 //});
 
 // run reverse traceroute (to req.ip)
-app.all('/tr', function(req, res){
-    var cmd = 'mtr'; 
-    cmd += _.map(req.params, function(v,k) {
-	return '-'+k+' '+v;
+app.all('/mtr', function(req, res){
+    var cmd = 'mtr '; 
+
+    cmd += _.map(req.query, function(v,k) {
+	var tmp = '-'+k+' '+v;
+	return "'" + tmp.replace(/'/g,"\'") + "'";
     }).join(' ');
-    if (cmd.indexOf('[;,$]')) {
-	res.status(500).send({ error : 'invalid request'});
-    }
-    cmd += ' ' + req.ip;
+    cmd += ' --raw ' + req.ip;
+
+    var result = {
+	ts : Date.now(),
+	cmd : 'mtr',
+	cmdline : cmd,
+	os : os.platform()
+    };
 
     exec(cmd, function(err, stdout, stderr) {
-    });
+	if (err || !stdout || stdout.length < 1) {
+	    result.error = { 
+		type : 'execerror',
+		message : stderr || stdout,
+		code : err
+	    };
+	} else {
+	    var r = {
+		dst: req.ip,
+		nqueries : (req.query.c ? parseInt(req.query.c) : 10),
+		hops: []
+	    };
+
+	    var lines = (stdout ? stdout.trim() : "").split("\n");
+	    for (var i = 0; i < lines.length; i++) {
+		var tmp = lines[i].trim().split(' ');
+		var hopid = parseInt(tmp[1]);
+		switch (tmp[0]) {
+		case 'h':
+		    r.hops[hopid] = { 
+			address : tmp[2],
+			hostname : undefined, 
+			missed : r.nqueries, 
+			rtt : [] 
+		    };
+		    break;
+
+		case 'p':
+		    var hop = r.hops[hopid];
+		    hop.missed -= 1;
+		    hop.rtt.push(parseInt(tmp[2])/1000.0);
+		    break;
+
+		case 'd':
+		    var hop = r.hops[hopid];
+		    hop.hostname = tmp[2];
+		    break;
+		}
+	    }
+	    // did we reach the destination ?
+	    r.succ = (r.hops.length > 0 && 
+		      (r.hops[r.hops.length-1].address === r.dst ||
+		       r.hops[r.hops.length-1].hostname === r.dst));
+
+	    // trim off the last dupl hop (if succ)
+	    if (r.hops.length > 1 && 
+		(r.hops[r.hops.length-1].address === 
+		 r.hops[r.hops.length-1].address)) 
+	    {
+		r.hops = r.hops.slice(0,r.hops.length-1)
+	    }
+	    result.result = r;
+	}
+	res.status(200).send(result);
+    }); // exec
 });
 
 // run reverse ping (to req.ip)
 app.all('/ping', function(req, res){
-    var cmd = 'ping'; 
-    if (!req.params.c)
-	req.params.c = 5;
+    var cmd = 'ping '; 
 
-    cmd += _.map(req.params, function(v,k) {
-	return '-'+k+' '+v;
+    if (!req.query.c)
+	req.query.c = 5;
+
+    cmd += _.map(req.query, function(v,k) {
+	var tmp = '-'+k+' '+v;
+	return "'" + tmp.replace(/'/g,"\'") + "'";
     }).join(' ');
-    if (cmd.indexOf('[;,$]')) {
-	res.status(500).send({ error : 'invalid request'});
-    }
     cmd += ' ' + req.ip;
 
     var result = {
 	ts : Date.now(),
 	cmd : 'ping',
 	cmdline : cmd,
-	os : os.platform(),
-	result : undefined
+	os : os.platform()
     };
 
-    var child = exec(cmd, function(err, stdout, stderr) {
+    exec(cmd, function(err, stdout, stderr) {
 	if (err || !stdout || stdout.length < 1) {
 	    result.error = { 
-		type : 'execerror'
+		type : 'execerror',
 		message : stderr || stdout,
-		code : err,
-		cmd : 'ping'
+		code : err
 	    };
 	} else {
 	    var lines = (stdout ? stdout.trim() : "").split("\n");
 	    var r = {
 		dst: req.ip,
 		dst_ip : req.ip,
-		count : req.params['c'],        // -c
-		bytes : req.params['b'],        // -b or default
-		ttl : req.params['t'], 
+		count : req.query['c'],        // -c
+		bytes : req.query['b'] || 56,  // -b or default
+		ttl : req.query['t'], 
 		lost : 0,                       // lost pkts
 		rtt : [],                       // results
 		stats : undefined,              // rtt stats
@@ -199,14 +260,18 @@ app.all('/ping', function(req, res){
 		alt : []                        // full responses
 	    };
 
-	    for (var i = 0; i < lines.length; i++) {	    
-		var line = lines[i].trim().replace(/\s+/g, ' ').split(' ');
+	    var i, line;
+	    for (i = 0; i < lines.length; i++) {	    
+		line = lines[i].trim().replace(/\s+/g, ' ').split(' ');
+
 		if (lines[i].toLowerCase().indexOf('time to live exceeded')>=0) {
 		    r.time_exceeded_from = line[1];
-		    break;		    
+		    break;
+		    
 		} else if (line[0] === 'PING') {
 		    r.dst_ip = line[2].replace(/\(|\)|:/gi, '');
 		    r.bytes = parseInt((line[3].indexOf('\(') < 0 ? line[3] : line[3].substring(0,line[3].indexOf('\('))));
+
 		} else if (line[1] === 'bytes') {
 		    for (var j = 2; j < line.length; j++) {
 			if (line[j].indexOf('time=') === 0) {
@@ -219,9 +284,8 @@ app.all('/ping', function(req, res){
 	    result.result = r;
 	}
 	res.status(200).send(result);
-    });
+    }); // exec
 });
-
 
 // startup
 var server = app.listen(port, function() {
